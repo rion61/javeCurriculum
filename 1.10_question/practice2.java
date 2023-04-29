e10dc59ce1bdb7abfe138cda8b9ff5d5828db2df
rion61 <ikdarion55@gmail.com>
2023-04-30 00:01:59 +0900

    課題1.10作成 提出 更新

---
+++ 1.10_question/practice2.java
@@ -0,0 +1,17 @@
+/*	
+問題.for文を使用して九九の計算を出力してください。
+	※出力例 	1×1=1 1×2=2…1×9=9
+				2×1=2 2×2=4…2×9=18
+						・
+						・
+						・
+				9×1=9 9×2=18…9×9=81
+				*/
+package practice_for.java;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+	
+	}
+}
