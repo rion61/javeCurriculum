103b630bdd230c2a843ecea9703e009687a8b8e6
rion61 <ikdarion55@gmail.com>
2023-05-13 21:47:41 +0900

    java Qes4 提出 修正更新

--- java/src/curriculum_B/Qes4.java
+++ java/src/curriculum_B/Qes4.java
@@ -2,12 +2,17 @@ package curriculum_B;
 
 public class Qes4 {
 	public static void main(String[] args) {
+		// 左側の繰り返し処理
 		for(int i = 1; i <= 9; i++) {
+			// 右側の繰り返し処理
 			for(int j = 1; j <= 9; j++){
+				// 0埋め
 				String formatStr = String.format("%02d", i);
 				String formatStr2 = String.format("%02d", j);
 				String formatStr3 = String.format("%02d", i * j);
+				// メッセージ表示
 				System.out.print(formatStr + "×" + formatStr2 + "=" + formatStr3);
+				// 9までかけたらスキップ
 				if (j == 9) {
 					continue;
 				}
