bf3f11be528caa49b2ef024e449a73d0d36433cf
rion61 <ikdarion55@gmail.com>
2023-07-24 12:37:20 +0900

    課題 java_30 新規提出

---
+++ java/src/java1_30/Main.java
@@ -0,0 +1,9 @@
+package java1_30;
+
+class Main {
+	public static void main(String[] args) {
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		person1.print();
+
+	}
+}
---
+++ java/src/java1_30/Person.java
@@ -0,0 +1,39 @@
+package java1_30;
+
+class Person{
+	// インスタンスフィールドを定義
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	static int count = 0;
+
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	public Person(String name, int age, double height, double weight){
+		// 名前
+		this.name = name;
+		// 年齢
+		this.age = age;
+		// 身長
+		this.height = height;
+		// 体重
+		this.weight = weight;
+
+	}
+
+	public double bmi(){
+		// bmi式
+		double bmi = weight / (height * height);
+		return bmi;
+	}
+
+	public  void print() {
+		// メッセージ表示
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this. age+ "です");
+		System.out.println("BMIは" + String.format("%.1f", bmi()) + "です");
+		count++;
+		System.out.println("合計" + count + "人です");
+
+	}
+}
