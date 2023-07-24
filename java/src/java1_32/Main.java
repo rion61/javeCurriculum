e98adb18535d4122e7f406c649e9500614443e87
rion61 <ikdarion55@gmail.com>
2023-07-24 12:42:31 +0900

    課題 java_32 新作成 提出

---
+++ java/src/java1_32/Main.java
@@ -0,0 +1,12 @@
+package java1_32;
+
+class Main {
+	public static void main(String[] args) {
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60.0);
+		person1.print();
+
+		Person person2 = new Person("山田花子", 22, 1.5, 40.0);
+		person2.print();
+		Person.printCount();
+	}
+}
---
+++ java/src/java1_32/Person.java
@@ -0,0 +1,40 @@
+package java1_32;
+
+class Person{
+	// インスタンスフィールドを定義
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	static int count = 0;
+
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	public Person(String name, int age, double height, double weight){
+		// 名前
+		this.name = name;
+		// 年齢
+		this.age = age;
+		// 身長
+		this.height = height;
+		// 体重
+		this.weight = weight;
+
+	}
+
+	public double bmi(){
+		// bmi式
+		double bmi = weight / (height * height);
+		return bmi;
+	}
+
+	public void print(){
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "です");
+		count++;
+		System.out.println("合計" + count + "人です");
+	}
+	
+	 static void printCount(){
+		System.out.println("合計" + count + "人です");
+	}
+}
