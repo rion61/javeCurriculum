c8ac33b406fc0f21a76e1a56853b191caaa85220
rion61 <ikdarion55@gmail.com>
2023-07-27 21:48:17 +0900

    課題 java1_25 再提出

--- java/src/java1_25/inheritanceSub.java
+++ java/src/java1_25/inheritanceSub.java
@@ -6,13 +6,13 @@ public class inheritanceSub extends inheritance{
 
 	public void status() {
 		Random rand = new Random();
-		inheritance nums = new inheritance();
+		
 		// ステータスの数
 				for(int i = 0; i <= 4; i++) {
 					// ランダムな数値
-					nums.setStatus(rand.nextInt(1000));
+					setStatus(rand.nextInt(1000));
 					// メッセージ表示
-					System.out.println(super.statusName[i] + nums.getStatus());
+					System.out.println(super.statusName[i] + getStatus());
 				}
 				// メッセージ表示
 				System.out.println("\nさあ冒険に出かけよう！");
